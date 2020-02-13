0e45b0ef041ff84bae888c24eccaf7ae5ee648db
Angga VB <anggakun90@gmail.com>
2020-02-14 04:32:02 +0700

    db brandplus

---
+++ wp-content/themes/flatsome-child/custom/picking-list.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Template Name: [custom] Picking List Form
+*/
+
+$user = wp_get_current_user();
+
+if ( !in_array( 'vendor', (array) $user->roles ) ) {
+	echo "<script>window.location = '".site_url()."/login-vendor/'</script>";
+}
+
+?>
+
+<?php get_header(); ?>
+
+<h1>Picking List</h1>
+
+<?php get_footer(); ?>
